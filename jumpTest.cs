669553c387b2c98ca90ed6ab775d3fce86eca4b9
이수민 <min001017@naver.com>
2021-07-17 02:37:03 +0900

    애니메이션 없이 이동 간단히 구현

---
+++ jumpTest.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpTest : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private Vector3 direction;
+    private Rigidbody rb;
+    private bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction.Normalize();
+
+        transform.Translate(direction * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) //충돌 감지 - 바닥에 닿았는지
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+}
